e7fe519fcd35d60de86bebb5f8c98896f85ce86a
sartr <sartr@DESKTOP-TDUMQJF>
2021-02-05 19:59:38 +0100

    enum toegevoegd

--- Models/Filament.cs
+++ Models/Filament.cs
@@ -5,20 +5,28 @@ using System.Threading.Tasks;
 
 namespace Kostenberekening_3D_printer_ASPnet.Models
 {
+    public enum Types
+    {
+        PLA,
+        ABS,
+        PETG,
+        TPE
+    }
+    public enum Kleuren
+    {
+        Roze,
+        Geel,
+        Wit,
+        Oranje,
+        Blauw,
+        Groen,
+        Transparant
+    }
     public class Filament
     {
-        string Type { get; set; }
-        decimal KostPerRol { get; set; }
-        int AantalMeter { get; set; }
-        public decimal KostprijsPrint { get; set; }
-
-        public Filament(string type, decimal kostPerRol)
-        {
-            //In program.cs is Type vastgezet als PLA
-            Type = type;
-            KostPerRol = kostPerRol;
-            AantalMeter = 330;
-        }
+        public Types Type { get; set; }
+        public decimal KostPerRol { get; set; }
+        public int AantalMeter { get => 330; }
 
         public decimal KostPerMeter()
         {
